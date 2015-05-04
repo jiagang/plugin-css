e97b5740a4efc4dd49a586211e3da23d0af0fa0e
jiagang <jiagang1985@gmail.com>
2015-05-04 13:37:13 +0800

    support cleancss rebase

--- css-builder.js
+++ css-builder.js
@@ -29,7 +29,8 @@ module.exports = function bundle(loads, opts) {
   var cleanCSS = new CleanCSS({
     target: loader.separateCSS ? opts.outFile : rootURL,
     relativeTo: rootURL,
-    sourceMap: opts.sourceMaps
+    sourceMap: opts.sourceMaps,
+    rebase: opts.cssRebase === false ? false : true,
   }).minify(loads.map(function(load) {
     return load.address.substr('file:'.length);
   }));
